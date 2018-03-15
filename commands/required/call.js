49a7d7bfc955b38d8a226dbe3311549b284ea4a7
Dev-Osmium <oatmas64134@gmail.com>
2018-03-15 09:48:44 -0400

    Remove the FOR loop and just make a new one goddammit

--- commands/required/call.js
+++ commands/required/call.js
@@ -23,15 +23,12 @@ module.exports = class CallModCommand extends Command {
         const guild = this.client.guilds.find(400734359226810378);
         const channel = guild.channels.find(400769467606368256);
         console.log('Fetching invites');
-        var members = guild.fetchMembers();
-        for(var i = 0; i<members.length; i++) {
-           var invites = guild.fetchInvites(parseInt(members[i].id));
-           console.log(invites[i].code);
-           /*if (invites.length != 0) {
-               var invite = invites[i];
-               return;
-           }*/
-        }
-        channel.send(`${message.author} needs help in ${message.guild.name} with invite ${invite.get}`);
+        var inviteChannel = message.guild.systemChannel;
+        var invite = inviteChannel.createInvite({
+            maxUses: 1,
+            reason: 'Someone called for a moderator!'
+        });
+        
+        channel.send(`${message.author} needs help in ${message.guild.name} with invite https://discord.gg/${invite.code}`);
     }
 }
