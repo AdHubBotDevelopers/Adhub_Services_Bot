fbfa5d253f60c7fdeeddfb253781bbb3c3a20310
Dev-Osmium <oatmas64134@gmail.com>
2018-03-15 09:44:19 -0400

    This time it loops through all the members and checks the invite. Not exactly efficient for big servers

--- commands/required/call.js
+++ commands/required/call.js
@@ -23,9 +23,15 @@ module.exports = class CallModCommand extends Command {
         const guild = this.client.guilds.find(400734359226810378);
         const channel = guild.channels.find(400769467606368256);
         console.log('Fetching invites');
-        message.guild.fetchInvites()
-            .then(invites => console.log(`Grabbed ${invites.get(0)} from a Collection with a size of ${invites.size}`))
-            .catch(console.error);
-        channel.send(`${message.author} needs help in ${message.guild.name} with invite ${invite.get(0)}`);
+        var members = guild.fetchMembers();
+        for(var i = 0; i<members.length; i++) {
+           var invites = guild.fetchInvites(parseInt(members[i].id));
+           console.log(invites[i].code);
+           /*if (invites.length != 0) {
+               var invite = invites[i];
+               return;
+           }*/
+        }
+        channel.send(`${message.author} needs help in ${message.guild.name} with invite ${invite.get}`);
     }
 }
